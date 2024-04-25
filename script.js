938f10bf08dfb0a452f1f30895a80c6f979984c0
Mahek Mahesh <144465044+mhkmhsh@users.noreply.github.com>
2024-04-25 17:25:02 +0000

    starting to add in changing things

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ const text1 = document.getElementById('para1')
 const text2 = document.getElementById('para2')
 
 function overview(){
-
+    title.innerHTML = 'Overview of the Vietnam War'
+img1.src = 'images/vietnam1.jpg'
 }
 function strategies(){
-    
+    title.innerHTML = 'Political and Military Strategies'
+    img1.innerHTML = 'images/vietnam4.jpg'
 }
 function opinion(){
-    
+    title.innerHTML = 'Home Front and Public Opinion'
+    img1.innerHTML = ''
 }
 function resistance(){
-    
+    title.innerHTML = 'Protests and Resistance'
+    img1.innerHTML = ''
 }
 function aftermath(){
-    
+    title.innerHTML = 'Global Impact and Aftermath'
+    img1.innerHTML = ''
 }
 function learned(){
-    
+    title.innerHTML = 'Global Impact and Aftermath'
+    img1.innerHTML = ''
 }
