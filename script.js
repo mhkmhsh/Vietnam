ec935ad0c1ba763d71d5760735a38fcfdcdfc62b
Mahek Mahesh <144465044+mhkmhsh@users.noreply.github.com>
2024-04-25 17:39:04 +0000

    got very far

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ const img2 = document.getElementById('image2')
 const text1 = document.getElementById('para1')
 const text2 = document.getElementById('para2')
 
-function overview(){
+function overview() {
     title.innerHTML = 'Overview of the Vietnam War'
-img1.src = 'images/vietnam1.jpg'
+    img1.src = 'images/vietnam1.jpg'
+    text1.innerHTML = `<b>Causes of the war:</b>The United States had major conflicts
+     politically and militarily with Vietnam that led to the war.
+      The four major causes of the war were the politics from the Cold War,
+       The Gulf of Tonkin, the power struggles between France and the Ho Chi Minh, 
+       and The Domino Theory. Due to the administration of John F. Kennedy, the President at the time,
+        the United States involvement became more and more drastic as troops were forced to participate in
+         battle and acquire supplies and weaponry.`
+    
 }
-function strategies(){
+function strategies() {
     title.innerHTML = 'Political and Military Strategies'
     img1.src = 'images/vietnam4.jpg'
+    text1.innerHTML = `<b>Guerrilla warfare tactics:</b>The Guerrilla warfare tactics were implemented in order to overthrow a political system through the use of war, violence, and terrorism. In addition, these smaller forces would battle against much larger forces and would strike early before the opponents would appear. The methods used in order to complete these attacks also included non-combat bombings, weapon attacks, hit and runs, and massive strikes. 
+    `
 }
-function opinion(){
+function opinion() {
     title.innerHTML = 'Home Front and Public Opinion'
     img1.src = 'images/vietnam7.jpeg'
+    text1.innerHTML = ``
 }
-function resistance(){
+function resistance() {
     title.innerHTML = 'Protests and Resistance'
     img1.src = 'images/vietnam8.jpg'
+    text1.innerHTML = ``
 }
-function aftermath(){
+function aftermath() {
     title.innerHTML = 'Global Impact and Aftermath'
     img1.src = 'images/vietnam10.jpg'
+    text1.innerHTML = ``
 }
-function learned(){
+function learned() {
     title.innerHTML = 'Global Impact and Aftermath'
     img1.src = 'images/vietnam12.jpg'
+    text1.innerHTML = ``
 }
