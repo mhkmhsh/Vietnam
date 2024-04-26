83aa8caf79e08629ce99b602005e7d5e5ecde3d3
Mahek Mahesh <144465044+mhkmhsh@users.noreply.github.com>
2024-04-26 18:43:52 +0000

    Saving near the end of the block

--- script.js
+++ script.js
@@ -40,15 +40,37 @@ function opinion() {
 function resistance() {
     title.innerHTML = 'Protests and Resistance'
     img1.src = 'images/vietnam8.jpg'
-    text1.innerHTML = ``
+    text1.innerHTML = ` <b>Student movements and organizations:</b>
+Due to the massive spread of propaganda, media coverage would display
+     false news and would affect public perspective on the ideas and realities of the war. Each country 
+     believed theirs was doing the right thing and followed what the media displayed to the public. 
+     In addition, some citizens began to feel betrayed by their nations due to the negative sources
+      they would receive and lies from the government. While there was a variety of negative public opinions,
+       the horrors and devastation soldiers would face while in battle caught the attention of American citizens
+        and they would come together in order to support their troops and provide them with any care they needed.
+    `
 }
 function aftermath() {
     title.innerHTML = 'Global Impact and Aftermath'
     img1.src = 'images/vietnam10.jpg'
-    text1.innerHTML = ``
+    text1.innerHTML = `<b> The wars impact on Vietnam and neighboring countries:</b> The war greatly impacted Vietnam and its neighboring countries.
+     All of the damage caused, costs of these damages, bombings, and naturally made toxics
+      formed irreparable loss. For instance, the use of Agent Orange and extremely specific 
+      bombings were incredibly elaborate while deadly. The number of deaths of not only civilians
+       and soldiers caused citizens to flee their home country in order to protect their families 
+       and escape persecution.`
 }
 function learned() {
-    title.innerHTML = 'Global Impact and Aftermath'
+    title.innerHTML = 'Reflections and Lessons Learned'
     img1.src = 'images/vietnam12.jpg'
-    text1.innerHTML = ``
+    text1.innerHTML = `<b>The wars place in historical memory:</b>The war caused a dramatic change in American history and will always be remembered for 
+    the number of battles and soldiers that gave their life to defend their country. Citizens began to lose faith
+     in the government, develop their own organizations to protect the war, and build stronger relationships with 
+     other nations while worsening them with others. Families strived to keep their loved ones safe and pray for
+      their protection if they had any relatives fighting in the war. These battles also increased the purchase 
+      of weapons in the military and usage of bombs and tools. Soldiers learned how to skillfully attack with 
+      dangerous weapons. Lastly, the cost of the weapons, damages, and medical care caused by the war was 
+      extremely overwhelming. Families were unable to pay for repairs and the government struggled to pay 
+      to restore the land and civilian homes. 
+    `
 }
